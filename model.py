be66f730dfdb7616ca08e8285e45ecd5fee49919
DanielaBreitman <daniela.breitman@mail.mcgill.ca>
2023-11-01 18:22:53 +0100

    Basic classes

---
+++ model.py
@@ -0,0 +1,12 @@
+from torch import nn
+class Linear(nn.Module):
+    pass
+class WordEmbedding(nn.Module):
+    pass
+class Dropout(nn.Module):
+    pass
+class PositionalEmbedding(nn.Module):
+    pass
+class MultiheadAttention(nn.Module):
+    pass
+
