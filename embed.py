577e35a3b390a62eebbb06e49d06d7c45f4f09dd
JSpencerPittman <jspencerpittman@gmail.com>
2023-11-02 16:50:37 -0500

    Minor error fix

--- embed.py
+++ embed.py
@@ -1,20 +1,19 @@
 import torch.nn as nn
 import torch
-import numpy as np
 
 class WordEmbedder(object):
     def __init__(self, word_dictionary, d_hidden):
         self.word_dictionary = word_dictionary
         self.d_hidden = d_hidden
 
-        self.embed_layer = nn.Linear(word_dictionary.size(), d_hidden)
-        self.unembed_layer = nn.Linear(d_hidden, word_dictionary.size())
+        self.embed_layer = nn.Linear(len(word_dictionary), d_hidden)
+        self.unembed_layer = nn.Linear(d_hidden, len(word_dictionary))
 
     def embed(self, tokens):
         d_t = len(tokens)
         tokens = [self.word_dictionary.to_num(t) for t in tokens]
 
-        one_hot = np.zeros(d_t, len(self.word_dictionary))
+        one_hot = torch.zeros((d_t, len(self.word_dictionary)))
 
         for t in range(d_t):
             one_hot[t][tokens[t]] = 1
@@ -27,8 +26,8 @@ class WordEmbedder(object):
 
         one_hot = self.unembed_layer(X)
 
-        res = np.argmax(one_hot, axis=1)
-        res = [self.word_dictionary.to_word(res) for n in res]
+        res = torch.argmax(one_hot, axis=1)
+        res = [self.word_dictionary.to_word(n) for n in res]
 
         return res
 
@@ -40,12 +39,9 @@ class WordDictionary(object):
 
     def add(self, word):
         if word not in self.word2num:
-            self.count += 1
             self.word2num[word] = self.count
             self.num2word[self.count] = word
-
-    def size(self):
-        return self.count
+            self.count+1
     
     def to_num(self, word):
         return self.word2num[word]
